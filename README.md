973edcce92689ade9830bc68135aaf903de354bf
Richard Hpa <richard.hpa@hotmail.com>
2020-01-14 09:31:22 +1300

    added in components from the react tutorial

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Testing in React JS
 
 A small project for learning how to create tests for a React application.  
-This repo follows the [React:Testing and Debugging](https://www.lynda.com/React-js-tutorials/Welcome/592511/626777-4.html) tutorial on Lynda.com by Emmanuel Henri.
+This repo follows the [React: Testing and Debugging](https://www.lynda.com/React-js-tutorials/Welcome/592511/626777-4.html) tutorial on Lynda.com by Emmanuel Henri.
 
 ## Installation
 ```bash
---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import courses from '../data/courses.json';
+import Grid from './listings/Grid';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {courses};
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="navbar-fixed">
+                    <nav className="blue lighten-2">
+                        <div className="nav-wrapper">
+                            <a href="./" className="brand-logo center">Courses</a>
+                        </div>
+                        </nav>
+                </div>
+                <div>
+                    <Grid items={this.state.courses}/>
+                </div>
+            </div>
+        );
+    }
+}
