3f8d86ae8f2cad1872241198ecd36dfc7055aa97
Richard Hpa <richard.hpa@hotmail.com>
2020-01-14 12:04:13 +1300

    added in 2 tests to check for strings

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -9,3 +9,12 @@ test('Number of items should equal 12', () => {
 test('Number of items to be Greater than 12', () => {
     expect(numItems).toBeGreaterThanOrEqual(12);
 })
+
+const dataTest = data[0].title;
+test('Checking there is a JS in the title', () => {
+    expect(dataTest).toMatch(/JS/);
+})
+
+test('The title contains React', () => {
+    expect(dataTest).toContain('React');
+})
