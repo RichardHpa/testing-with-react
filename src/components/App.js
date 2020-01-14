24d33b70401e6cdeba09a4d1e299d5eb57389b95
Richard Hpa <richard.hpa@hotmail.com>
2020-01-14 14:18:37 +1300

    fixed error with the single test

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Grid from './listings/Grid';
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {courses};
+        this.state = { courses };
     }
 
     render() {
